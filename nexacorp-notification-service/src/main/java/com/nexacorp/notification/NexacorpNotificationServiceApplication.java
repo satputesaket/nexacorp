06573b22e9b6e8c51312c4ec907e172ed0a8ae7d
satputesaket <satputesaket@gmail.com>
2026-06-18 12:24:25 +0530

    added notification service

---
+++ nexacorp-notification-service/src/main/java/com/nexacorp/notification/NexacorpNotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nexacorp.notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexacorpNotificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexacorpNotificationServiceApplication.class, args);
+	}
+
+}
---
+++ nexacorp-notification-service/src/test/java/com/nexacorp/notification/NexacorpNotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexacorp.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexacorpNotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
